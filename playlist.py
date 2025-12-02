c206a06301eb970e213264ac7e1685437e24b12a
Trần Phương Nhi <nhi05244@gmail.com>
2025-12-02 07:53:49 +0700

    Thêm bài hát

--- playlist.py
+++ playlist.py
@@ -1,5 +1,19 @@
 songs = []
 
+def add_song():
+  title = input("Nhập tên bài hát: ")
+  artist = input("Nhập tên ca sĩ: ")
+  duration = int(input("Nhập thời lượng (giây): "))
+
+  songs.append({
+    'title': title,
+    'artist': artist,
+    'duration': duration
+  })
+
+  print("Đã thêm bài hát vào playlist.")
+
+
 def main():
   while True:
     print("\n--- MUSIC PLAYLIST MANAGER ---")
